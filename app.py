ede66aab74450c184432d32bb9220e53e71d9834
ManuelLopezB <manuelalejandro.lopezbarrios@gmail.com>
2022-03-13 20:56:31 -0500

    avance domingo 13 de marzo

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
