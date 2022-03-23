36cbdcc1a3513afac1122b355486b8b005fa330d
ManuelLopezB <manuelalejandro.lopezbarrios@gmail.com>
2022-03-23 10:38:22 -0500

    blink test

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from gpiozero import LED
+from signal import pause
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/blink")
+def blink():
+    led = LED(17)
+    led.blink()
+ 
